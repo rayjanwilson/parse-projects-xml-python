89842f2b83fa09fba601df2e980879dcb8f581d2
Rayjan Wilson <rayjan.wilson@gmail.com>
2017-03-17 13:23:53 -0400

    adds a guard clause to determine if we should append the xml, in case our stuff is already there

--- parse-it.py
+++ parse-it.py
@@ -25,14 +25,9 @@ def parse_folder_name(folderName):
 
     nodeName = folderName.split('CPV_')[1]
 
-    firstLine = "<add key=\"UseFixedLogName\" value=\"True\" />"
-    secondLine = "<add key=\"FixedLogName\" value=\"E:\\CPV\\{}_impexp_{}_CPV_ACTON\\EX-CPV_ACTON.log\" />".format(projectNameUpper,projectNameLower)
-
     parsed = {}
     parsed['Project'] = projectName
     parsed['Node'] = nodeName
-    parsed['FirstLine'] = firstLine
-    parsed['SecondLine'] = secondLine
 
     return parsed
 
@@ -72,11 +67,11 @@ def indent(elem, level=0):
             elem.tail = i
 
 def write_file(directory, parsed):
+    print('Project: {}'.format(parsed['Project']))
     xml_orig = 'CPV_{}_batchCPVimpexp.xml'.format(parsed['Project'].lower())
     xml_file = os.path.join(directory, xml_orig)
 
     if os.path.isfile(xml_file):
-        print(xml_file)
         tree = xml.ElementTree(file=xml_file)
         root = tree.getroot()
         appSettings = root.find('appSettings')
@@ -84,11 +79,23 @@ def write_file(directory, parsed):
         log = xml.Element("add", {"key":"UseFixedLogName", "value":"True"})
         val = "E:\\CPV\\{}_impexp_{}_CPV_ACTON\\EX-CPV_ACTON.log".format(parsed['Project'].upper(), parsed['Project'].lower())
         log2 = xml.Element("add", {"key":"FixedLogName", "value":val})
-        appSettings.append(log)
-        appSettings.append(log2)
-        tree = xml.ElementTree(root)
-        indent(root)
-        tree.write(xml_file, xml_declaration=True)
+
+        should_we_append = True
+
+        # check to see if the attribute is already there for some reason
+        for child in appSettings:
+            if child.attrib == log.attrib:
+                should_we_append = False
+
+        if should_we_append:
+            print('Updating: {}'.format(xml_file))
+            appSettings.append(log)
+            appSettings.append(log2)
+            tree = xml.ElementTree(root)
+            indent(root)
+            tree.write(xml_file, xml_declaration=True)
+        else:
+            print('Skipping: {}'.format(xml_file))
     else:
         print('FAIL:: {} doesnt exists'.format(xml_file))
 
@@ -103,7 +110,6 @@ def main(args):
 
         if(args.write):
             if 'ARCADIA' in d:
-                print(abs_path_d)
                 write_file(abs_path_d, parsed_folder)
 
 
