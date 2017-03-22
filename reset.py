a012b04f649e7e203c17890dc3ebb782b3967b6c
Rayjan Wilson <rayjan.wilson@gmail.com>
2017-03-22 14:04:04 -0400

    adds the test for the alternative xml file name

--- reset.py
+++ reset.py
@@ -21,10 +21,15 @@ if __name__ == '__main__':
 
     test_folder = './test/folders'
     abs_path_test_folder = os.path.abspath(test_folder)
-    example_file_name = 'CPV_lametro_batchCPVimpexp.xml'
+
     example_file_loc = './test/input/'
     dirs = get_dirs(abs_path_test_folder)
     for d in dirs:
+        example_file_name = ''
+        if 'dukenet' in d:
+            example_file_name = 'CPV_dukenet_BatchCPVImpExp.xml'
+        else:
+            example_file_name = 'CPV_lametro_batchCPVimpexp.xml'
         src = os.path.join(example_file_loc, example_file_name)
         dst = os.path.join(abs_path_test_folder, d, example_file_name)
         if(args.verbose):
